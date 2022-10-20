438bbb1af7bfaac4977f686237fcf11e9911edf6
johnturnham <support@wallet.inc>
2022-10-20 00:16:26 +0000

    v2.1.515

--- dist/model/applicableTerminalsAnyOf4.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ApplicableTerminalsAnyOf4 = void 0;
-var ApplicableTerminalsAnyOf4;
-(function (ApplicableTerminalsAnyOf4) {
-    ApplicableTerminalsAnyOf4[ApplicableTerminalsAnyOf4["Wix"] = 'Wix'] = "Wix";
-})(ApplicableTerminalsAnyOf4 = exports.ApplicableTerminalsAnyOf4 || (exports.ApplicableTerminalsAnyOf4 = {}));
-//# sourceMappingURL=applicableTerminalsAnyOf4.js.map
