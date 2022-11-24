ead54ce95ef58834da2a281658247cefb6618a9c
johnturnham <support@wallet.inc>
2022-11-24 02:16:34 +0000

    v2.1.524

--- dist/model/statusAnyOf3.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.StatusAnyOf3 = void 0;
-var StatusAnyOf3;
-(function (StatusAnyOf3) {
-    StatusAnyOf3[StatusAnyOf3["Refunded"] = 'refunded'] = "Refunded";
-})(StatusAnyOf3 = exports.StatusAnyOf3 || (exports.StatusAnyOf3 = {}));
-//# sourceMappingURL=statusAnyOf3.js.map
