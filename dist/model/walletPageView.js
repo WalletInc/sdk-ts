e718e0b94c0dfbc222d85abb5c3222241622fc2d
johnturnham <support@wallet.inc>
2023-06-27 22:25:36 +0000

    v2.1.535

--- dist/model/walletPageView.js
+++ dist/model/walletPageView.js
@@ -100,6 +100,11 @@ WalletPageView.attributeTypeMap = [
         "type": "string"
     },
     {
+        "name": "referringDomain",
+        "baseName": "referringDomain",
+        "type": "string"
+    },
+    {
         "name": "referrer",
         "baseName": "referrer",
         "type": "string"
