2b64a9a64e0f4da974933fe9e71b1235def7d4af
johnturnham <support@wallet.inc>
2026-06-29 22:05:27 +0000

    v2.1.602

--- dist/examples/constants.d.ts
+++ dist/examples/constants.d.ts
@@ -1,4 +1,4 @@
-export declare const domain = "https://dev.api.wall.et";
+export declare const domain = "https://api.wall.et";
 export declare const headers: {
     headers: {
         "access-token": string;
--- dist/examples/constants.js
+++ dist/examples/constants.js
@@ -4,7 +4,6 @@ exports.headers = exports.domain = void 0;
 exports.domain = "https://api.wall.et";
 exports.headers = {
     headers: {
-        // Create your API key in the Wallet Developer Hub (https://wallet.dev) and paste it here.
         "access-token": "YOUR_API_KEY"
     }
 };
