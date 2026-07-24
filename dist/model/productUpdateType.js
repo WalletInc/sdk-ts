d07fa92d77c4c3f66a29550c01df0f726d872ec8
johnturnham <support@wallet.inc>
2026-07-24 12:04:42 +0000

    v2.4.1

---
+++ dist/model/productUpdateType.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProductUpdateType = void 0;
+class ProductUpdateType {
+    static getAttributeTypeMap() {
+        return ProductUpdateType.attributeTypeMap;
+    }
+}
+exports.ProductUpdateType = ProductUpdateType;
+ProductUpdateType.discriminator = undefined;
+ProductUpdateType.attributeTypeMap = [];
+//# sourceMappingURL=productUpdateType.js.map
