b9bb26d4a9c89e86e84d6f1db1b45fb40d28201d
johnturnham <support@wallet.inc>
2026-07-25 14:05:02 +0000

    v2.18.0

--- dist/model/subscriptionFeature.d.ts
+++ dist/model/subscriptionFeature.d.ts
@@ -1,13 +1,14 @@
 export declare class SubscriptionFeature extends null<String, any> {
-    'sortNum': number;
-    'name': string;
-    'maxVolume'?: string;
-    'measurement': string;
-    'description': string;
-    'currentVolume'?: string;
-    'isExceeded'?: boolean;
-    'isInUse'?: boolean;
-    'isEnabled'?: boolean;
+    'sortNum': any | null;
+    'name': any | null;
+    'slug'?: any | null;
+    'maxVolume'?: any | null;
+    'measurement': any | null;
+    'description': any | null;
+    'currentVolume'?: any | null;
+    'isExceeded'?: any | null;
+    'isInUse'?: any | null;
+    'isEnabled'?: any | null;
     static discriminator: string | undefined;
     static attributeTypeMap: Array<{
         name: string;
