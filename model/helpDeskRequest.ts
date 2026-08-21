76b81b1c2c5c1405621c6215b5fb4f796afab032
johnturnham <support@wallet.inc>
2026-08-21 16:06:48 +0000

    v2.38.0

--- model/helpDeskRequest.ts
+++ model/helpDeskRequest.ts
@@ -1,6 +1,6 @@
 /**
  * wallet-api
- * Wallet Inc. API reference.  **Spec version 2.38.0**, built 2026-08-21T03:57:20.504Z
+ * Wallet Inc. API reference.  **Spec version 2.38.0**, built 2026-08-21T16:02:07.706Z
  *
  * The version of the OpenAPI document: 2.38.0
  * Contact: development@wallet.inc
